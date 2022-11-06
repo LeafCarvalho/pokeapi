748f5850832ea346d804e84c9c06d9836fb1413f
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2022-11-05 22:14:29 -0300

    Inserindo modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <div class="modal">
+        <div class="cards">
             <li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
@@ -17,18 +17,52 @@ function convertPokemonToLi(pokemon) {
                     </ol>
                     <img src="${pokemon.photo}"
                          alt="${pokemon.name}">
-                    <button>Saiba mais</button>
+                    <button onclick="More()" id="button">Saiba mais</button>
                 </div>
             </li>
         </div>
+
+        <div class="modal-container">
+        <div class="modal">
+        <span class="name">${pokemon.name}</span>
+          <hr />
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting 
+            industry. Lorem Ipsum has been the industry's standard dummy text
+             ever since the 1500s, when an unknown printer took a galley of 
+             type and scrambled it to make a type specimen book. 
+          </span>
+          <hr />
+          <div class="btns">
+            <button class="btnOK" onclick="closeMore()">OK</button>
+            <button class="btnClose" onclick="closeMore()">Close</button>
+          </div>
+        </div>
+      </div>
+
     `
 }
 
-function loadPokemonItens(offset, limit) {
+
+const loadPokemonItens = (offset, limit) => {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
     })
+}
+
+/* Abrir o card de informações */
+
+const More = () => {
+    const modalContainer = document.querySelector('.modal-container')
+    modalContainer.classList.add('active')
+}
+
+/* Fechar o card de informações */
+
+const closeMore = () => {
+    const modalContainer = document.querySelector('.modal-container')
+    modalContainer.classList.remove('active')
 }
 
 loadPokemonItens(offset, limit)
@@ -47,8 +81,7 @@ loadMoreButton.addEventListener('click', () => {
     }
 })
 
-const modal = () => {
-    let modal = document.querySelector('.modal')
-
-    modal.style.display = 'block'
+const cards = () => {
+    let cards = document.querySelector('.cards')
+    cards.style.display = 'block'
 }
