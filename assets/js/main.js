abd1f9fa5b78271d2ae2538b8d9548c5f37a278e
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2022-11-07 21:17:37 -0300

    projeto base completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,71 +7,34 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <div class="cards">
-            <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-                    <img src="${pokemon.photo}"
-                         alt="${pokemon.name}">
-                    <button onclick="More()" id="button">Saiba mais</button>
-                </div>
-            </li>
-        </div>
-
-        <div class="modal-container">
-        <div class="modal">
-        <span class="name">${pokemon.name}</span>
-          <hr />
-          <span>
-            Lorem Ipsum is simply dummy text of the printing and typesetting 
-            industry. Lorem Ipsum has been the industry's standard dummy text
-             ever since the 1500s, when an unknown printer took a galley of 
-             type and scrambled it to make a type specimen book. 
-          </span>
-          <hr />
-          <div class="btns">
-            <button class="btnOK" onclick="closeMore()">OK</button>
-            <button class="btnClose" onclick="closeMore()">Close</button>
-          </div>
-        </div>
-      </div>
-
+        <li class="pokemon ${pokemon.type}" onclick="openModal(${pokemon.number})">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
     `
 }
 
-
-const loadPokemonItens = (offset, limit) => {
+function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
     })
 }
 
-/* Abrir o card de informações */
-
-const More = () => {
-    const modalContainer = document.querySelector('.modal-container')
-    modalContainer.classList.add('active')
-}
-
-/* Fechar o card de informações */
-
-const closeMore = () => {
-    const modalContainer = document.querySelector('.modal-container')
-    modalContainer.classList.remove('active')
-}
-
 loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    const qtdRecordsWithNextPage = offset + limit
+    const qtdRecordsWithNexPage = offset + limit
 
-    if (qtdRecordsWithNextPage >= maxRecords) {
+    if (qtdRecordsWithNexPage >= maxRecords) {
         const newLimit = maxRecords - offset
         loadPokemonItens(offset, newLimit)
 
@@ -80,8 +43,3 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
-
-const cards = () => {
-    let cards = document.querySelector('.cards')
-    cards.style.display = 'block'
-}
